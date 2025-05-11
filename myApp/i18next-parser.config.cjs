8d0b3d34de1a4e122c6a749fe8dc6801863b494f
DaniIliev <114102749+DaniIliev@users.noreply.github.com>
2025-05-11 23:54:16 +0300

    translate

--- myApp/i18next-parser.config.cjs
+++ myApp/i18next-parser.config.cjs
@@ -1,16 +1,29 @@
+// module.exports = {
+//     locales: ['eng', 'bg'],
+//     defaultNamespace: 'translation',
+//     // output: 'public/locales/$LOCALE/$NAMESPACE.json',
+//     // output: 'src/locales/$LOCALE/$NAMESPACE.json',
+//     output: 'src/locales/$LOCALE.json', 
+//     createOldCatalogs: false,
+//     lexers: {
+//       js: ['JavascriptLexer'],
+//       jsx: ['JsxLexer'],
+//     },
+//     keySeparator: '.', // keys like 'home.title'
+//     namespaceSeparator: ':',
+//     useKeysAsDefaultValue: true,
+//   };
 module.exports = {
-    locales: ['eng', 'bg'],
-    defaultNamespace: 'translation',
-    // output: 'public/locales/$LOCALE/$NAMESPACE.json',
-    // output: 'src/locales/$LOCALE/$NAMESPACE.json',
-    output: 'src/locales/$LOCALE.json', 
-    createOldCatalogs: false,
-    lexers: {
-      js: ['JavascriptLexer'],
-      jsx: ['JsxLexer'],
-    },
-    keySeparator: '.', // keys like 'home.title'
-    namespaceSeparator: ':',
-    useKeysAsDefaultValue: true,
-  };
+  locales: ['eng', 'bg'],
+  defaultNamespace: 'translation',
+  output: 'src/locales/$LOCALE.json', // Точно както го имаш
+  createOldCatalogs: false,
+  keySeparator: false, // 👈 ВАЖНО! Защото използваш обикновени ключове като "Sign in"
+  namespaceSeparator: false, // 👈 Също важно, няма 'namespace:key'
+  useKeysAsDefaultValue: true,
+  lexers: {
+    js: [{ lexer: 'JavascriptLexer', functions: ['t'] }],
+    jsx: [{ lexer: 'JsxLexer', functions: ['t'] }],
+  },
+};
   
