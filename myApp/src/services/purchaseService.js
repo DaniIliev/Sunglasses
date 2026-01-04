f51ad423ec195e327d7d6c9f2b9818a44971f026
DaniIliev <danieljivkov04@abv.bg>
2026-01-04 14:03:15 +0200

    delete and mark as read

--- myApp/src/services/purchaseService.js
+++ myApp/src/services/purchaseService.js
@@ -1,41 +1,39 @@
 import { REACT_APP_API_URL } from "../env";
 
-const apiUrl = REACT_APP_API_URL; 
+const apiUrl = REACT_APP_API_URL;
 
 export const createPurchase = async (formData) => {
-    try {
-        const response = await fetch(`${apiUrl}/purchase`, {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify(formData),
-        });
-  
-        if (!response.ok) {
-          const errorData = await response.json();
-          console.error("Грешка от сървъра:", errorData);
-          return;
-        }
-        const data = await response.json();
-        alert("Тhe order is successful");
-        return data;
-      } catch (error) {
-        console.error("Грешка при fetch:", error);
-      }
-}
-export const getAll = async () => {
-    const data = await fetch(`${apiUrl}/purchase`,
-        {
-            method: 'GET',
-            credentials: 'include',
-        }
-    )
-    const purchase = await data.json()
+  try {
+    const response = await fetch(`${apiUrl}/purchase`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(formData),
+    });
 
-    return purchase
-}
+    if (!response.ok) {
+      const errorData = await response.json();
+      console.error("Грешка от сървъра:", errorData);
+      return;
+    }
+    const data = await response.json();
+    alert("Тhe order is successful");
+    return data;
+  } catch (error) {
+    console.error("Грешка при fetch:", error);
+  }
+};
+export const getAll = async () => {
+  const data = await fetch(`${apiUrl}/purchase`, {
+    method: "GET",
+    credentials: "include",
+  });
+  const purchase = await data.json();
+
+  return purchase;
+};
 
 export const getPurchaseById = async (id) => {
-  try{
+  try {
     const response = await fetch(`${apiUrl}/purchase/${id}`, {
       method: "GET",
       headers: { "Content-Type": "application/json" },
@@ -48,10 +46,10 @@ export const getPurchaseById = async (id) => {
     }
     const data = await response.json();
     return data;
-  }catch(error){
+  } catch (error) {
     console.error("Грешка при fetch:", error);
   }
-}
+};
 
 export const markAsSeen = async (id) => {
   try {
@@ -93,4 +91,3 @@ export const deletePurchase = async (id) => {
     return null;
   }
 };
-
--- service/controller/purchaseController.js
+++ service/controller/purchaseController.js
@@ -1,8 +1,8 @@
-const router = require('express').Router()
-const Purchase = require('../schemas/PurchaseSchema')
-const mongoose = require("mongoose")
+const router = require("express").Router();
+const Purchase = require("../schemas/PurchaseSchema");
+const mongoose = require("mongoose");
 
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   const newPurchase = new Purchase({
     seen: req.body.seen ?? false,
     additionalInfo: req.body.additionalInfo,
@@ -19,48 +19,49 @@ router.post('/', async (req, res) => {
     totalPurchasePrice: req.body.totalPurchasePrice,
     zipCode: req.body.zipCode,
     orderCode: req.body.orderCode,
-    purchaseDate: req.body.purchaseDate
+    purchaseDate: req.body.purchaseDate,
   });
-    try{
-        await newPurchase.save();
-        res.status(201).json(newPurchase);
-    } catch (error) {
-        console.error('Error saving sunglasses:', error); // Логване на грешката
-        res.status(500).json({ message: 'Грешка при добавяне на слънчевите очила', error });
-    }
-})
-
-router.get('/', async (req, res) => {
-    try {
-        const purchases = await Purchase.find({});
-        res.status(200).json(purchases);
-    } catch (error) {
-        res.status(500).json({ message: 'Error fetching sunglasses', error });
-    }
+  try {
+    await newPurchase.save();
+    res.status(201).json(newPurchase);
+  } catch (error) {
+    console.error("Error saving sunglasses:", error); // Логване на грешката
+    res
+      .status(500)
+      .json({ message: "Грешка при добавяне на слънчевите очила", error });
+  }
 });
 
-router.get('/:id', async (req,res) => {
-    const {id} = req.params;
-    
-    try {
-      if (!mongoose.Types.ObjectId.isValid(id)) {
-        return res.status(400).json({ message: "Invalid ID format" });
-      }
-  
-       const purchase = await Purchase.findById(id);
-      if (!purchase) {
-        return res.status(404).json({ message: "Purchase not found" });
-      }
-  
-    
-      res.status(200).json(purchase);
-    } catch (error) {
-      console.error("Error fetching purchase:", error);
-      res.status(500).json({ message: "Server error", error });
-    }
-  }); 
+router.get("/", async (req, res) => {
+  try {
+    const purchases = await Purchase.find({});
+    res.status(200).json(purchases);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching sunglasses", error });
+  }
+});
 
-router.patch('/:id/seen', async (req, res) => {
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({ message: "Invalid ID format" });
+    }
+
+    const purchase = await Purchase.findById(id);
+    if (!purchase) {
+      return res.status(404).json({ message: "Purchase not found" });
+    }
+
+    res.status(200).json(purchase);
+  } catch (error) {
+    console.error("Error fetching purchase:", error);
+    res.status(500).json({ message: "Server error", error });
+  }
+});
+
+router.patch("/:id/seen", async (req, res) => {
   const { id } = req.params;
   const { seen = true } = req.body;
 
@@ -86,7 +87,7 @@ router.patch('/:id/seen', async (req, res) => {
   }
 });
 
-router.delete('/:id', async (req, res) => {
+router.delete("/:id", async (req, res) => {
   const { id } = req.params;
 
   try {
@@ -106,4 +107,4 @@ router.delete('/:id', async (req, res) => {
     res.status(500).json({ message: "Server error", error });
   }
 });
-module.exports = router
+module.exports = router;
